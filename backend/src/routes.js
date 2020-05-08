1b45f035daebebbf3254051158991019f5dc4cc5
Everton Costa <evertonsouzaesc@gmail.com>
2020-05-08 03:05:13 -0300

    Remove old backend

--- backend/src/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const LoginController = require('./controllers/LoginController');
-
-const routes = express.Router();
-
-routes.post('/login', LoginController.app);
